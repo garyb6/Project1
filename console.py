109c56791ed32a69c44bc850c40386c784c80c35
garyb6 <gary.bennett6@outlook.com>
2021-12-12 18:30:44 +0000

    countries and stadiums added to console

--- console.py
+++ console.py
@@ -1,4 +1,4 @@
-from models.stadium import Book
+from models.stadium import Stadium
 from models.country import Country
 
 import repositories.stadium_repository as stadium_repository
@@ -8,3 +8,26 @@ stadium_repository.delete_all()
 country_repository.delete_all() 
 
 # need to add countries and stadiums 
+
+country1 = Country("Australia", "English", False)
+country_repository.save(country1)
+
+country2 = Country("Portugal", "Portguese", True)
+country_repository.save(country2)
+
+country3 = Country("Germany", "German", True)
+country_repository.save(country3)
+
+stadium1 = Stadium("The Gabba", "Cricket", country1, False)
+stadium_repository.save(stadium1)
+
+stadium2 = Stadium("Estádio do Dragão", "Football", country2, False)
+stadium_repository.save(stadium2)
+
+stadium3 = Stadium("Allianz Arena", "Football", country3, False)
+stadium_repository.save(stadium3)
+
+stadium4 = Stadium("Red Bull Arena", "Football", country3, False)
+stadium_repository.save(stadium4) 
+
+

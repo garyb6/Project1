c9e475e37a99d30ebcb43b3cf0f5ec7bd3a9a77a
garyb6 <gary.bennett6@outlook.com>
2021-12-13 10:52:29 +0000

    show html displays Hello Pet - countries

--- controllers/country_controller.py
+++ controllers/country_controller.py
@@ -10,4 +10,24 @@ def countries():
     countries = country_repository.select_all()
     return render_template("countries/index.html", all_countries = countries)
 
-# @countries_blueprint.route()
+#NEW
+@countries_blueprint.route("/countries/new", methods=['GET'])
+def new_country():
+    return render_template("country/new.html", all_countries = countries)
+
+# # CREATE
+# @stadiums_blueprint.route("/stadiums",  methods=['POST'])
+# def create_stadium():
+#     name    = request.form['name']
+#     category = request.form['category']
+#     country  = country_repository.select(request.form['country_id'])
+#     visited = request.form['visited']
+#     stadium = Stadium(name, category, country, visited)
+#     stadium_repository.save(stadium)
+#     return redirect('/stadiums')
+
+# SHOW??
+@countries_blueprint.route("/countries/<id>", methods=['GET'])
+def show_countries(id):
+    country = country_repository.select(id)
+    return render_template('countries/show.html', country = country)

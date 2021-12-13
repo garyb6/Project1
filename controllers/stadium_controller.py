a0032e1217640a0f91aeedc0dfd90882da577251
garyb6 <gary.bennett6@outlook.com>
2021-12-13 10:24:20 +0000

    edited stadium controller file

--- controllers/stadium_controller.py
+++ controllers/stadium_controller.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request, redirect
 from flask import Blueprint
 from models.stadium import Stadium
 import repositories.stadium_repository as stadium_repository
+import repositories.country_repository as country_repository 
 
 stadiums_blueprint = Blueprint("stadiums", __name__)
 
@@ -9,3 +10,55 @@ stadiums_blueprint = Blueprint("stadiums", __name__)
 def stadiums():
     stadiums = stadium_repository.select_all()
     return render_template("stadiums/index.html", all_stadiums = stadiums)
+
+#NEW
+@stadiums_blueprint.route("/stadiums/new", methods=['GET'])
+def new_stadium():
+    countries = country_repository.select_all()
+    return render_template("stadium/new.html", all_countries = countries)
+
+# CREATE
+@stadiums_blueprint.route("/stadiums",  methods=['POST'])
+def create_stadium():
+    name    = request.form['name']
+    category = request.form['category']
+    country  = country_repository.select(request.form['country_id'])
+    visited = request.form['visited']
+    stadium = Stadium(name, category, country, visited)
+    stadium_repository.save(stadium)
+    return redirect('/stadiums')
+
+# SHOW??
+@stadiums_blueprint.route("/stadiums/<id>", methods=['GET'])
+def show_stadium(id):
+    stadium = stadium_repository.select(id)
+    return render_template('books/show.html', stadium = stadium)
+
+# EDIT
+# GET '/stadiums/<id>/edit'
+@stadiums_blueprint.route("/stadiums/<id>/edit", methods=['GET'])
+def edit_stadium(id):
+    stadium = stadium_repository.select(id)
+    countries = country_repository.select_all()
+    return render_template('stadiums/edit.html', stadium = stadium, all_countries = countries)
+
+# UPDATE
+# PUT '/stadiums/<id>'
+@stadiums_blueprint.route("/stadiums/<id>", methods=['POST'])
+def update_stadium(id):
+    name = request.form['name']
+    category = request.form['category']
+    country  = country_repository.select(request.form['country_id'])
+    visited = request.form['visited']
+    stadium = Stadium(name, category, country, visited, id)
+    print(stadium.country.name())
+    stadium_repository.update(stadium)
+    return redirect('/stadiums')
+
+# DELETE
+# DELETE '/books/<id>'
+@stadiums_blueprint.route("/stadiums/<id>/delete", methods=['POST'])
+def delete_stadium(id):
+    stadium_repository.delete(id)
+    return redirect('/') 
+#might need to change the redirect 

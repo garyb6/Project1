5491cecff364a11986c2c892edbd3038dff63416
garyb6 <gary.bennett6@outlook.com>
2021-12-15 23:15:01 +0000

    small formatting changes and deleting dead code

--- controllers/countries_controller.py
+++ controllers/countries_controller.py
@@ -10,12 +10,10 @@ def countries():
     countries = country_repository.select_all()
     return render_template("countries/index.html", all_countries = countries)
 
-#NEW
 @countries_blueprint.route("/countries/new", methods=['GET'])
 def new_country():
     return render_template("countries/new.html")
 
-# CREATE
 @countries_blueprint.route("/countries",  methods=['POST'])
 def create_country():
     name    = request.form['name']
@@ -25,19 +23,16 @@ def create_country():
     country_repository.save(country)
     return redirect('/countries')
 
-# SHOW
 @countries_blueprint.route("/countries/<id>", methods=['GET'])
 def show_countries(id):
     country = country_repository.select(id)
     return render_template('countries/show.html', country = country)
 
-# EDIT
 @countries_blueprint.route("/countries/<id>/edit", methods=['GET'])
 def edit_country(id):
     country = country_repository.select(id)
     return render_template('countries/edit.html', country = country)
 
-# UPDATE
 @countries_blueprint.route("/countries/<id>", methods=['POST'])
 def update_country(id):
     name = request.form['name']
@@ -46,13 +41,6 @@ def update_country(id):
     country = Country(name, language, visited, id)
     country_repository.update(country)
     return redirect('/countries')
-
-# # DELETE
-# # DELETE '/stadiums/<id>'
-# @stadiums_blueprint.route("/stadiums/<id>/delete", methods=['POST'])
-# def delete_stadium(id):
-#     stadium_repository.delete(id)
-#     return redirect('/') 
 
 @countries_blueprint.route("/countries/")
 def countries_visited():

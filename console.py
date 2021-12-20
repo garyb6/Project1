58a6a41a3e7479f5493072c89d4bb1b5ffc04b26
garyb6 <gary.bennett6@outlook.com>
2021-12-20 14:57:57 +0000

    can now add countries after refactor

--- console.py
+++ console.py
@@ -4,8 +4,8 @@ from models.country import Country
 import repositories.stadium_repository as stadium_repository
 import repositories.country_repository as country_repository 
 
-# stadium_repository.delete_all() 
-# country_repository.delete_all() 
+stadium_repository.delete_all() 
+country_repository.delete_all() 
 
 
 country1 = Country("Australia", "Oceania", "English", "Kings of the Ashes unfortunately", False, 2)

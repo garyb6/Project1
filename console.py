1ba7bd1d2a89d29bdd9112904970b808d6e0e3e9
garyb6 <gary.bennett6@outlook.com>
2021-12-20 13:37:59 +0000

    console.py refactored

--- console.py
+++ console.py
@@ -8,25 +8,25 @@ stadium_repository.delete_all()
 country_repository.delete_all() 
 
 
-country1 = Country("Australia", "English", False)
+country1 = Country("Australia", "Oceania", "English", "Kings of the Ashes unfortunately", False, 2)
 country_repository.save(country1)
 
-country2 = Country("Portugal", "Portuguese", True)
+country2 = Country("Portugal", "Europe", "Portuguese", "Home of the Algarve", True, 4)
 country_repository.save(country2)
 
-country3 = Country("Germany", "German", True)
+country3 = Country("Germany", "Europe", "German", "Best football stadiums in the world", True, 5)
 country_repository.save(country3)
 
-stadium1 = Stadium("The Gabba", "Cricket", country1, False)
+stadium1 = Stadium("The Gabba", "Cricket", "Australia's fortress", "Brisbane", country1, False, 5)
 stadium_repository.save(stadium1)
 
-stadium2 = Stadium("Estádio do Dragão", "Football", country2, False)
+stadium2 = Stadium("Estádio do Dragão", "Football", "Stadium of Light", "Porto", country2, False, 3)
 stadium_repository.save(stadium2)
 
-stadium3 = Stadium("Allianz Arena", "Football", country3, False)
+stadium3 = Stadium("Allianz Arena", "Football", "Home of the Champions", "Munich", country3, False, 4)
 stadium_repository.save(stadium3)
 
-stadium4 = Stadium("Red Bull Arena", "Football", country3, False)
+stadium4 = Stadium("Red Bull Arena", "Football", "Commercialisation of football", "Leipzig", country3, False, 1)
 stadium_repository.save(stadium4) 
 
 
